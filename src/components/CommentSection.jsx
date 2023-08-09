0014df51301260929d8fd363b4ebd1a474b715fa
alexcoman1 <octavianalexandru.coman@zebra.com>
2023-08-09 21:13:41 +0200

    update

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,6 +1,7 @@
 import CommentPost from './CommentPost';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import axios from 'axios'
+import { UserContext } from '../../context/userContext';
 
 
 
@@ -8,6 +9,7 @@ export default function CommentSection() {
     const [comments, setComments] = useState([]);
     const [text, setText] = useState('');
     const [loading, setLoading] = useState(true)
+    const { user } = useContext(UserContext)
     
 
     useEffect(() => {
@@ -57,6 +59,32 @@ export default function CommentSection() {
         
     }
 
+    const handleDelete = async (commentId) => {
+        const getCookie = (name) => {
+            const value = `; ${document.cookie}`;
+            const parts = value.split(`; ${name}=`);
+            if (parts.length === 2) return parts.pop().split(';').shift();
+            return null;
+        }
+    
+        const token = getCookie('token');
+        console.log('Retrieved token:', token)
+        
+        try {
+            await axios.delete(`/comments/${commentId}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+                withCredentials: true
+            })
+            setComments(comments.filter(comment => comment._id !== commentId))
+        } catch (error) {
+            console.error('An error occured while deleting a comment:', error)
+        }
+    }
+
+    
+
     if (loading) return <div>Loading...</div>
 
     return (
@@ -69,12 +97,23 @@ export default function CommentSection() {
             </form>
             {comments.slice().reverse().map((comment, index) => (
                 <div key={comment._id} className="comment-post">
+
+                    {console.log("Comment author ID:", comment.author && comment.author._id)}
+                    {console.log("User ID from context:", user && user._id)}
+                    {console.log("User object from context:", user)}
+
                     <p className="author-name">{comment.author ? comment.author.name : 'Anonymous'}</p>
                     <CommentPost {...comment} />
+                    {comment.author && comment.author._id === user.id && (
+                        <button onClick={() => handleDelete(comment._id)}>Delete</button>
+                    )}
                 </div>
+                
             ))}
         </div>
 
 
     )
+
+    
 }
